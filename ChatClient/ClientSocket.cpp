d444249b3b43577ebbba2cfa0787d0d97737cac6
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-04-10 19:35:58 +0900

    MyClientSocket-cpp

---
+++ ChatClient/ClientSocket.cpp
@@ -0,0 +1,28 @@
+﻿// ClientSocket.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ChatClient.h"
+#include "ClientSocket.h"
+
+
+// ClientSocket
+
+ClientSocket::ClientSocket()
+{
+}
+
+ClientSocket::~ClientSocket()
+{
+}
+
+
+// ClientSocket 멤버 함수
+
+
+void ClientSocket::OnReceive(int nErrorCode)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	AfxGetMainWnd()->PostMessage(UM_RECEIVE, 0, (LPARAM)this);		// PreTranslateMessage 사용 위해 Post로 메세지 보냄
+	CSocket::OnReceive(nErrorCode);
+}
---
+++ ChatClient/ClientSocket.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+// ClientSocket 명령 대상
+#define UM_RECEIVE (WM_USER + 1)
+class ClientSocket : public CSocket
+{
+public:
+	ClientSocket();
+	virtual ~ClientSocket();
+
+	virtual void OnReceive(int nErrorCode);
+};
+
+
