016af145ffb3a4b98fdac4c16fb2c6004112aff8
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-04-10 19:36:49 +0900

    Control-h

---
+++ ChatClient/resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ChatClient.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDD_CHATCLIENT_DIALOG           103
+#define IDR_MAINFRAME                   128
+#define IDC_RECEIVE_DATA                1000
+#define IDC_SEND_DATA                   1001
+#define IDC_USER_LIST                   1002
+#define IDC_FORCED_EXIT_BT              1003
+#define IDC_SEND_BT                     1004
+#define IDC_START_BT                    1005
+#define IDC_STOP_BT                     1006
+#define IDC_IP_ADDRESS                  1007
+#define IDC_SERVER_PORT                 1008
+#define IDC_NAME                        1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
